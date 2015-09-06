e04c323420c65371197037a8d87c80ba49e8dd96
AnnColor <Anna@Anns-MacBook.Dlink>
2015-09-06 22:38:44 +0200

    try install gulp and clear gulpfile
    start first step

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,6 @@ var gulp = require('gulp'),
 	browserSync = require('browser-sync').create(),
 	reload = browserSync.reload;
 
-var params = {
-	out: 'public',
-	htmlSrc: 'index.html',
-	level: ['common.blocks']
-};
-
-gulp.task('default',['server'],['build']);
-
-gulp.task('server',function(){
-	browserSync.init({
-		server: params.out
-	});
-});
-
-gulp.task('build',['html']);
-
-gulp.task('html', function(){
-	gulp.src(params.htmlSrc)
-	.pipe(rename('index.html'))
-	.pipe(gulp.dest(params.out));
+gulp.task('default', function() {
+  // place code for your default task here
 });
