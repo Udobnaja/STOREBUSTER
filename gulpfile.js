5c964a03541142c146ccb706d142a58220fd92af
Udobnaja <Udobnaja@gmail.com>
2015-09-17 21:38:23 +0200

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,37 @@ var gulp = require('gulp'),
 	browserSync = require('browser-sync').create(),
 	reload = browserSync.reload;
 
-gulp.task('default', function() {
-  // place code for your default task here
+var params = {
+    out : 'public',
+    htmlSrc : 'index.html',
+    levels : 'css'
+};
+
+gulp.task('default', ['server','build']);
+
+//поднимаем сервер
+gulp.task('server', function(){
+    browserSync.init({
+        server : params.out
+    });
+    //смотрим за изменениями в html  выполняем команду html
+    gulp.watch('*.html', ['html']);
+});
+
+//Задача для сборки
+gulp.task('build', ['html', 'css']);
+
+//Сборка html файлов
+gulp.task('html', function(){
+    gulp.src(params.htmlSrc)
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest(params.out))
+    .pipe(reload({stream : true}));
+});
+
+gulp.task('css', function(){
+    gulp.src(['css/*.css'])
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest(params.out))
+    .pipe(reload({stream : true}));
 });
